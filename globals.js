51495ae491bdb1c124ef8236b69ea3f0725d4601
Tom Hinkle <tmhinkle@gmail.com>
2019-01-18 10:33:37 -0500

    ++fields for assignments and gradebooks

--- globals.js
+++ globals.js
@@ -1,1 +1,3 @@
-BUSY = 'busy'
+var BUSY = 'busy';
+var assignmentFields = ['GB column name','Assignment name','Category','Date assigned','Date due','Total points','Extra credit points','Grade Scale','Grade Term'];
+var gradebookFields = ['Class','Student Name','Assignment Name','Grade','Comment']
