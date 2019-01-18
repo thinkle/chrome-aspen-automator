906f66259a9cfa2960863cd5c26cb96727e65367
Tom Hinkle <tmhinkle@gmail.com>
2019-01-18 10:32:34 -0500

    Code for converting to/from bases. We make a base as big as we can encode in the character set X2 accepts - this lets us convert long numerical google IDs into 10-digit X2 text-encoded IDs

---
+++ base.js
@@ -0,0 +1,158 @@
+/* Let's get the latin codepoints... */
+
+BaseAsMuchAsWeCan = {
+    // print re.sub('[\s"\']','',''.join([unichr(n) for n in range(33,256)]))
+    _Rixits : "!#$%&'()*+,-./0123456789:;	=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~€‚ƒ„…†‡ˆ‰Š‹ŒŽ‘’“”•–—˜™š›œžŸ¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ",
+
+    fromNumber : function(number) {
+        if (isNaN(Number(number)) || number === null ||
+            number === Number.POSITIVE_INFINITY)
+            throw "The input is not valid";
+        if (number < 0)
+            throw "Can't represent negative numbers now";
+
+        var rixit; // like 'digit', only in some non-decimal radix 
+        var residual = Math.floor(number);
+        var result = '';
+        while (true) {
+            rixit = residual % this._Rixits.length
+            // console.log("rixit : " + rixit);
+            // console.log("result before : " + result);
+            result = this._Rixits.charAt(rixit) + result;
+            // console.log("result after : " + result);
+            // console.log("residual before : " + residual);
+            residual = Math.floor(residual / this._Rixits.length);
+            // console.log("residual after : " + residual);
+
+            if (residual == 0)
+                break;
+            }
+        return result;
+    },
+
+    toNumber : function(rixits) {
+        var result = 0;
+        // console.log("rixits : " + rixits);
+        // console.log("rixits.split('') : " + rixits.split(''));
+        rixits = rixits.split('');
+        for (var e = 0; e < rixits.length; e++) {
+            // console.log("_Rixits.indexOf(" + rixits[e] + ") : " + 
+                // this._Rixits.indexOf(rixits[e]));
+            // console.log("result before : " + result);
+            result = (result * this._Rixits.length) + this._Rixits.indexOf(rixits[e]);
+            // console.log("result after : " + result);
+        }
+        return result;
+    }
+}
+
+    
+
+Base128 = {
+
+    _Rixits :
+//   0       8       16      24      32      40      48      56     63
+//   v       v       v       v       v       v       v       v      v
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/~!@#$%^&*()_-{[}]|:;.,<>?āĂăĄąĆćĈĉĊċČčĎďȀȁȃȄȅȆȇȈȉȊЁЂЃЄЅԆ؇؈؉ЊЋЌЍЎ",
+    // YOU have the freedom, here, to choose the glyphs you want for 
+    // representing your base-64 numbers. The ASCII encoding guys usually
+    // choose a set of glyphs beginning with ABCD..., but, looking at
+    // your update #2, I deduce that you want glyphs beginning with 
+    // 0123..., which is a fine choice and aligns the first ten numbers
+    // in base 64 with the first ten numbers in decimal.
+
+    // This cannot handle negative numbers and only works on the 
+    //     integer part, discarding the fractional part.
+    // Doing better means deciding on whether you're just representing
+    // the subset of javascript numbers of twos-complement 32-bit integers 
+    // or going with base-64 representations for the bit pattern of the
+    // underlying IEEE floating-point number, or representing the mantissae
+    // and exponents separately, or some other possibility. For now, bail
+    fromNumber : function(number) {
+        if (isNaN(Number(number)) || number === null ||
+            number === Number.POSITIVE_INFINITY)
+            throw "The input is not valid";
+        if (number < 0)
+            throw "Can't represent negative numbers now";
+
+        var rixit; // like 'digit', only in some non-decimal radix 
+        var residual = Math.floor(number);
+        var result = '';
+        while (true) {
+            rixit = residual % 128
+            // console.log("rixit : " + rixit);
+            // console.log("result before : " + result);
+            result = this._Rixits.charAt(rixit) + result;
+            // console.log("result after : " + result);
+            // console.log("residual before : " + residual);
+            residual = Math.floor(residual / 128);
+            // console.log("residual after : " + residual);
+
+            if (residual == 0)
+                break;
+            }
+        return result;
+    },
+
+    toNumber : function(rixits) {
+        var result = 0;
+        // console.log("rixits : " + rixits);
+        // console.log("rixits.split('') : " + rixits.split(''));
+        rixits = rixits.split('');
+        for (var e = 0; e < rixits.length; e++) {
+            // console.log("_Rixits.indexOf(" + rixits[e] + ") : " + 
+                // this._Rixits.indexOf(rixits[e]));
+            // console.log("result before : " + result);
+            result = (result * 128) + this._Rixits.indexOf(rixits[e]);
+            // console.log("result after : " + result);
+        }
+        return result;
+    }
+}
+
+//print(''.join([unichr(i) for i in range(64,128)+range(256,512)+range(592,592+96)+range(913,930)+range(931,1024)+range(8704,8704+256)+range(8960,8960+128)+range(5024,5094)+range(6016,6060)]))
+
+Base1024 = {
+    _Rixits : "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķĸĹĺĻļĽľĿŀŁłŃńŅņŇňŉŊŋŌōŎŏŐőŒœŔŕŖŗŘřŚśŜŝŞşŠšŢţŤťŦŧŨũŪūŬŭŮůŰűŲųŴŵŶŷŸŹźŻżŽžſƀƁƂƃƄƅƆƇƈƉƊƋƌƍƎƏƐƑƒƓƔƕƖƗƘƙƚƛƜƝƞƟƠơƢƣƤƥƦƧƨƩƪƫƬƭƮƯưƱƲƳƴƵƶƷƸƹƺƻƼƽƾƿǀǁǂǃǄǅǆǇǈǉǊǋǌǍǎǏǐǑǒǓǔǕǖǗǘǙǚǛǜǝǞǟǠǡǢǣǤǥǦǧǨǩǪǫǬǭǮǯǰǱǲǳǴǵǶǷǸǹǺǻǼǽǾǿɐɑɒɓɔɕɖɗɘəɚɛɜɝɞɟɠɡɢɣɤɥɦɧɨɩɪɫɬɭɮɯɰɱɲɳɴɵɶɷɸɹɺɻɼɽɾɿʀʁʂʃʄʅʆʇʈʉʊʋʌʍʎʏʐʑʒʓʔʕʖʗʘʙʚʛʜʝʞʟʠʡʢʣʤʥʦʧʨʩʪʫʬʭʮʯΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩΪΫάέήίΰαβγδεζηθικλμνξοπρςστυφχψωϊϋόύώϏϐϑϒϓϔϕϖϗϘϙϚϛϜϝϞϟϠϡϢϣϤϥϦϧϨϩϪϫϬϭϮϯϰϱϲϳϴϵ϶ϷϸϹϺϻϼϽϾϿ∀∁∂∃∄∅∆∇∈∉∊∋∌∍∎∏∐∑−∓∔∕∖∗∘∙√∛∜∝∞∟∠∡∢∣∤∥∦∧∨∩∪∫∬∭∮∯∰∱∲∳∴∵∶∷∸∹∺∻∼∽∾∿≀≁≂≃≄≅≆≇≈≉≊≋≌≍≎≏≐≑≒≓≔≕≖≗≘≙≚≛≜≝≞≟≠≡≢≣≤≥≦≧≨≩≪≫≬≭≮≯≰≱≲≳≴≵≶≷≸≹≺≻≼≽≾≿⊀⊁⊂⊃⊄⊅⊆⊇⊈⊉⊊⊋⊌⊍⊎⊏⊐⊑⊒⊓⊔⊕⊖⊗⊘⊙⊚⊛⊜⊝⊞⊟⊠⊡⊢⊣⊤⊥⊦⊧⊨⊩⊪⊫⊬⊭⊮⊯⊰⊱⊲⊳⊴⊵⊶⊷⊸⊹⊺⊻⊼⊽⊾⊿⋀⋁⋂⋃⋄⋅⋆⋇⋈⋉⋊⋋⋌⋍⋎⋏⋐⋑⋒⋓⋔⋕⋖⋗⋘⋙⋚⋛⋜⋝⋞⋟⋠⋡⋢⋣⋤⋥⋦⋧⋨⋩⋪⋫⋬⋭⋮⋯⋰⋱⋲⋳⋴⋵⋶⋷⋸⋹⋺⋻⋼⋽⋾⋿⌀⌁⌂⌃⌄⌅⌆⌇⌈⌉⌊⌋⌌⌍⌎⌏⌐⌑⌒⌓⌔⌕⌖⌗⌘⌙⌚⌛⌜⌝⌞⌟⌠⌡⌢⌣⌤⌥⌦⌧⌨〈〉⌫⌬⌭⌮⌯⌰⌱⌲⌳⌴⌵⌶⌷⌸⌹⌺⌻⌼⌽⌾⌿⍀⍁⍂⍃⍄⍅⍆⍇⍈⍉⍊⍋⍌⍍⍎⍏⍐⍑⍒⍓⍔⍕⍖⍗⍘⍙⍚⍛⍜⍝⍞⍟⍠⍡⍢⍣⍤⍥⍦⍧⍨⍩⍪⍫⍬⍭⍮⍯⍰⍱⍲⍳⍴⍵⍶⍷⍸⍹⍺⍻⍼⍽⍾⍿ᎠᎡᎢᎣᎤᎥᎦᎧᎨᎩᎪᎫᎬᎭᎮᎯᎰᎱᎲᎳᎴᎵᎶᎷᎸᎹᎺᎻᎼᎽᎾᎿᏀᏁᏂᏃᏄᏅᏆᏇᏈᏉᏊᏋᏌᏍᏎᏏᏐᏑᏒᏓᏔᏕᏖᏗᏘᏙᏚᏛᏜᏝᏞᏟᏠᏡᏢᏣᏤᏥកខគឃងចឆជឈញដឋឌឍណតថទធនបផពភមយរលវឝឞសហឡអឣឤឥឦឧឨឩឪឫ",
+    fromNumber : function(number) {
+        if (isNaN(Number(number)) || number === null ||
+            number === Number.POSITIVE_INFINITY)
+            throw "The input is not valid";
+        if (number < 0)
+            throw "Can't represent negative numbers now";
+
+        var rixit; // like 'digit', only in some non-decimal radix 
+        var residual = Math.floor(number);
+        var result = '';
+        while (true) {
+            rixit = residual % 1024
+            // console.log("rixit : " + rixit);
+            // console.log("result before : " + result);
+            result = this._Rixits.charAt(rixit) + result;
+            // console.log("result after : " + result);
+            // console.log("residual before : " + residual);
+            residual = Math.floor(residual / 1024);
+            // console.log("residual after : " + residual);
+
+            if (residual == 0)
+                break;
+            }
+        return result;
+    },
+
+    toNumber : function(rixits) {
+        var result = 0;
+        // console.log("rixits : " + rixits);
+        // console.log("rixits.split('') : " + rixits.split(''));
+        rixits = rixits.split('');
+        for (var e = 0; e < rixits.length; e++) {
+            // console.log("_Rixits.indexOf(" + rixits[e] + ") : " + 
+                // this._Rixits.indexOf(rixits[e]));
+            // console.log("result before : " + result);
+            result = (result * 1024) + this._Rixits.indexOf(rixits[e]);
+            // console.log("result after : " + result);
+        }
+        return result;
+    }
+}
+    
